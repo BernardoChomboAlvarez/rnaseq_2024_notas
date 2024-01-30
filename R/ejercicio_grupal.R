a8815b6ec09da38af27d5dc9ce497824c627d07c
Bernardo Chombo Álvarez <bchombo@lcg.unam.mx>
2024-01-30 16:33:47 -0600

    Initial commit

--- R/ejercicio_grupal.R
+++ R/ejercicio_grupal.R
@@ -12,4 +12,4 @@
 # hapmapsnp6: https://bioconductor.org/packages/release/data/experiment/html/hapmapsnp6.html
 #
 # Opinión grupal:
-# Todos lo paquetes revisados cuentan con na buena documentación y atención a los usuarios, con excepción del de hapmapsnp6 que no está disponible para 2 sistemas operativos y la documentación es pésima. En lo personal, scider es el más padre y el más robusto en todos los parámetros.
+# Todos lo paquetes revisados cuentan con una buena documentación y atención a los usuarios, con excepción del de hapmapsnp6 que no está disponible para 2 sistemas operativos y la documentación es pésima. En lo personal, scider es el más padre y el más robusto en todos los parámetros.
