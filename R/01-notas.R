92d40eac6bdcd7f530c99d69ff3f93aa91b89c37
Bernardo Chombo Álvarez <bchombo@lcg.unam.mx>
2024-01-30 12:30:18 -0600

    Initial commit

---
+++ R/01-notas.R
@@ -0,0 +1,7 @@
+# First class
+# Creation of notes repo
+usethis::create_project("~/rnaseq_2024_notas")
+
+# Creation of first file tests
+## Creemos el archivo R/02-visualizar-mtcars.R
+usethis::use_r("02-visualizar-mtcars.R")
