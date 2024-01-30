f36e36b25ca64d87d6789b13e64c6f95caabc07e
Bernardo Chombo Álvarez <bchombo@lcg.unam.mx>
2024-01-30 13:59:38 -0600

    Initial commit

---
+++ R/ejercicio_grupal.R
@@ -0,0 +1,15 @@
+# Paquetes elegidos:
+#   scider
+#   scDesign3
+#
+# Opinion personal:
+# Ambos paquetes se ven bastante robustos con un amplio número de descargas. Los dos son para visualizar datos de scRNAseq, uno más enfocado a la densidad y otro enfocado a la visualización en 3D. Ambos paquetes contestan los issues levantados por los usuarios y en lo personal, scider es el que ofrece más visualizaciones y está mejor documentado.
+#
+# Paquetes elegidos:
+# iNETgrate: https://bioconductor.org/packages/iNETgrate
+# scider: https://bioconductor.org/packages/scider
+# scDesign3: https://bioconductor.org/packages/scDesign3
+# hapmapsnp6: https://bioconductor.org/packages/release/data/experiment/html/hapmapsnp6.html
+#
+# Opinión grupal:
+# Todos lo paquetes revisados cuentan con na buena documentación y atención a los usuarios, con excepción del de hapmapsnp6 que no está disponible para 2 sistemas operativos y la documentación es pésima. En lo personal, scider es el más padre y el más robusto en todos los parámetros.
